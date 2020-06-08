f3cd2a069bb56d2bb6c8c170855830ea98f26292
Víctor Velarde <victor.velarde@gmail.com>
2020-06-08 20:13:08 +0200

    feat: add basic reference generation with typedoc

---
+++ typedoc.js
@@ -0,0 +1,22 @@
+/**
+ * Configuration for reference, auto-generated from .ts files and their tsdoc comments
+ */
+module.exports = {
+  // excludeNotDocumented: true,
+  exclude: ['**/node_modules/**/*.ts', '**/__tests__/**/*', '**/*.types.ts'],
+  excludeExternals: true,
+  excludeNotExported: true,
+  excludePrivate: true,
+  excludeProtected: true,
+  ignoreCompilerErrors: true,
+  inputFiles: './packages',
+  listInvalidSymbolLinks: true,
+  mode: 'file',
+  module: 'es2015',
+  name: '@carto/web-sdk',
+  out: './docs/api',
+  plugin: 'none',
+  target: 'es6',
+  theme: 'default',
+  tsconfig: './tsconfig.json'
+};
