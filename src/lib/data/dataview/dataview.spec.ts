750770baf9c1a9152b90a060564a41bd89496aa5
Víctor Velarde <victor.velarde@gmail.com>
2020-06-10 17:35:58 +0200

    Add Data module
    Add Data module (skipping 1 event test due to issues with mitt)

---
+++ src/lib/data/dataview/dataview.spec.ts
@@ -0,0 +1,48 @@
+import { CartoError } from '../../core/errors/CartoError';
+import { Layer } from '../../viz/layer/Layer';
+import { DataView } from './dataview';
+
+jest.mock('mitt', () => ({
+  default: jest.fn()
+}));
+
+describe('DataView', () => {
+  describe('Instance Creation', () => {
+    it('should create new DataView instance', () => {
+      expect(() => new DataView(new Layer('fake_source'), 'fake_column')).not.toThrow();
+    });
+
+    it('should throw an exception when source is not provided', () => {
+      expect(() => new DataView(undefined as never, 'fake_column')).toThrow(
+        new CartoError({
+          type: '[DataView]',
+          message: 'Source was not provided while creating dataview'
+        })
+      );
+    });
+
+    it('should throw an exception when column is not provided', () => {
+      expect(() => new DataView(new Layer('fake_source'), undefined as never)).toThrow(
+        new CartoError({
+          type: '[DataView]',
+          message: 'Column name was not provided while creating dataview'
+        })
+      );
+    });
+  });
+
+  describe('Events', () => {
+    // TODO skip event test, until we figure out a way to manage mitt with ts-jest
+    it.skip('dataUpdate', () => {
+      const layer = new Layer('fake_source');
+      const dataView = new DataView(layer, 'popEst');
+
+      const dataUpdateSpy = jest.fn();
+      dataView.on('dataUpdate', dataUpdateSpy);
+
+      layer.emit('viewportLoad');
+
+      expect(dataUpdateSpy).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/lib/data/dataview/formula/formula.ts
@@ -0,0 +1,75 @@
+import { CartoError } from '../../../core/errors/CartoError';
+import { Layer } from '../../../viz/layer/Layer';
+import { CARTOSource } from '../../../viz/sources/CARTOSource';
+import { DataView } from '../dataview';
+import { AggregationType, aggregate } from '../../operations/aggregation/aggregation';
+
+export class FormulaDataView extends DataView {
+  private operation: AggregationType;
+
+  constructor(dataSource: CARTOSource | Layer, column: string, options: FormulaDataViewOptions) {
+    const { operation } = options || {};
+    validateParameters(operation, column);
+
+    super(dataSource, column);
+
+    this.operation = operation;
+  }
+
+  async getData() {
+    const features = await this.getSourceData([this.column]);
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const values = features.map((feature: { [x: string]: any }) => feature[this.column]);
+    validateNumbersOrNullIn(values);
+
+    // just include numbers in calculations... TODO: should we consider features with null & undefined for the column?
+    const filteredValues = values.filter(Number.isFinite);
+
+    return {
+      result: aggregate(filteredValues, this.operation),
+      operation: this.operation,
+      nullCount: values.length - filteredValues.length
+    };
+  }
+}
+
+function validateParameters(operation: AggregationType, column: string) {
+  if (!operation) {
+    throw new CartoError({
+      type: '[DataView]',
+      message:
+        'Operation property not provided while creating dataview. Please check documentation.'
+    });
+  }
+
+  if (!column) {
+    throw new CartoError({
+      type: '[DataView]',
+      message: 'Column property not provided while creating dataview. Please check documentation.'
+    });
+  }
+}
+
+/**
+ * Check the values are numbers or null | undefined, taking a small sample
+ * @param features
+ */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function validateNumbersOrNullIn(values: any[]) {
+  const sample = values.slice(0, Math.min(values.length, 10));
+  sample.forEach(value => {
+    const isAcceptedNull = value === null || value === undefined; // TODO should we just support null?
+
+    if (!isAcceptedNull && typeof value !== 'number') {
+      throw new CartoError({
+        type: '[DataView]',
+        message: `Column property for Formula can just contain numbers (or nulls) and a ${typeof value} with ${value} value was found. Please check documentation.`
+      });
+    }
+  });
+}
+
+interface FormulaDataViewOptions {
+  operation: AggregationType;
+}
