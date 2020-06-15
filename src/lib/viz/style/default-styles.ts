62bc35e0c29e79d36abf6883a6125c28c7ea0b35
Simon Martín <oleurud@gmail.com>
2020-06-15 15:20:29 +0200

    small change in point style defaults

--- src/lib/viz/style/default-styles.ts
+++ src/lib/viz/style/default-styles.ts
@@ -2,12 +2,12 @@ const NULL_COLOR = '#ccc';
 const OTHERS_COLOR = '#777';
 const NULL_SIZE = 0;
 const STROKE_WIDTH = 0.5;
-const STROKE_COLOR = '#22222288';
+const STROKE_COLOR = '#22222244';
 
 export const defaultStyles: any = {
   Point: {
     color: '#EE4D5A',
-    size: 10,
+    size: 8,
     opacity: 1,
     strokeColor: STROKE_COLOR,
     strokeWidth: STROKE_WIDTH,
