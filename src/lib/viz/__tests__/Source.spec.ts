7b024710bb30bc5f2f2f1f24678f3f3d222a71fe
Simon Martín <oleurud@gmail.com>
2020-06-19 19:07:33 +0200

    ensuring new shouldInitialize behavior in tests

--- src/lib/viz/__tests__/Source.spec.ts
+++ src/lib/viz/__tests__/Source.spec.ts
@@ -2,34 +2,40 @@ import { shouldInitialize } from '../sources/Source';
 
 describe('Source', () => {
   describe('shouldInitialize', () => {
+    const emptySet: Set<string> = new Set();
+    const filledSet: Set<string> = new Set(['field']);
+
     it('should initialize always for first time (isInitialized === false)', () => {
       const isInitialized = false;
-      const sample: Set<string> = new Set();
-      const aggregation: Set<string> = new Set();
-      const newFields = { sample, aggregation };
-      const currentFields: Set<string> = new Set();
+      const newFields = { sample: emptySet, aggregation: emptySet };
+      const currentFields = { sample: emptySet, aggregation: emptySet };
 
       const result = shouldInitialize(isInitialized, newFields, currentFields);
       expect(result).toEqual(true);
+    });
+
+    it('should not initialize again if all are equal', () => {
+      const isInitialized = true;
+      const newFields = { sample: emptySet, aggregation: emptySet };
+      const currentFields = { sample: emptySet, aggregation: emptySet };
+
+      const result = shouldInitialize(isInitialized, newFields, currentFields);
+      expect(result).toEqual(false);
     });
 
     it('should initialize with new fields', () => {
       const isInitialized = true;
-      const sample: Set<string> = new Set(['field']);
-      const aggregation: Set<string> = new Set();
-      const newFields = { sample, aggregation };
-      const currentFields: Set<string> = new Set();
+      const newFields = { sample: filledSet, aggregation: emptySet };
+      const currentFields = { sample: emptySet, aggregation: emptySet };
 
       const result = shouldInitialize(isInitialized, newFields, currentFields);
       expect(result).toEqual(true);
     });
 
-    it('should not initialize with same fields', () => {
+    it('should not initialize again with same fields', () => {
       const isInitialized = true;
-      const sample: Set<string> = new Set(['field']);
-      const aggregation: Set<string> = new Set();
-      const newFields = { sample, aggregation };
-      const currentFields: Set<string> = new Set(['field']);
+      const newFields = { sample: filledSet, aggregation: emptySet };
+      const currentFields = { sample: filledSet, aggregation: emptySet };
 
       const result = shouldInitialize(isInitialized, newFields, currentFields);
       expect(result).toEqual(false);
@@ -37,10 +43,8 @@ describe('Source', () => {
 
     it('should not initialize with less fields', () => {
       const isInitialized = true;
-      const sample: Set<string> = new Set(['field']);
-      const aggregation: Set<string> = new Set();
-      const newFields = { sample, aggregation };
-      const currentFields: Set<string> = new Set(['field']);
+      const newFields = { sample: filledSet, aggregation: emptySet };
+      const currentFields = { sample: filledSet, aggregation: filledSet };
 
       const result = shouldInitialize(isInitialized, newFields, currentFields);
       expect(result).toEqual(false);
