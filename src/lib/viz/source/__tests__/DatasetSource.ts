ab64d16abb32f2dae1b43e1320294d21b7bb4d6a
Simon Martín <oleurud@gmail.com>
2020-07-01 15:54:04 +0200

    mapConfig tests

--- src/lib/viz/source/__tests__/DatasetSource.ts
+++ src/lib/viz/source/__tests__/DatasetSource.ts
@@ -56,4 +56,125 @@ describe('DatasetSource', () => {
       }).rejects.toEqual(new Error('Error fake'));
     });
   });
+
+  describe('mapConfig', () => {
+    let instantiateMapFromMock: any;
+
+    beforeEach(() => {
+      setDefaultCredentials({ ...TEST_CREDENTIALS });
+
+      instantiateMapFromMock = jest.fn().mockImplementation(() => {
+        return {
+          metadata: {
+            url: {
+              vector: {
+                urlTemplate: '{s}.carto.com',
+                subdomains: []
+              }
+            },
+            layers: [
+              {
+                meta: {
+                  stats: {
+                    geometryType: 'Point'
+                  }
+                }
+              }
+            ]
+          }
+        };
+      });
+      Client.prototype.instantiateMapFrom = instantiateMapFromMock;
+    });
+
+    it('should have default mapConfig', async () => {
+      const source = new CARTOSource(DEFAULT_DATASET);
+      await source.init({ sample: new Set(), aggregation: new Set() });
+
+      const expectedmapConfig = {
+        vectorExtent: 2048,
+        vectorSimplifyExtent: 2048,
+        bufferSize: {
+          mvt: 10
+        },
+        dataset: DEFAULT_DATASET,
+        metadata: {
+          geometryType: true,
+          columnStats: {
+            topCategories: 32768,
+            includeNulls: true
+          },
+          dimensions: true,
+          sample: {
+            num_rows: 1000,
+            include_columns: []
+          }
+        },
+        aggregation: {
+          columns: {},
+          dimensions: {},
+          placement: 'centroid',
+          resolution: 1,
+          threshold: 1
+        }
+      };
+
+      expect(instantiateMapFromMock.mock.calls[0][0]).toMatchObject(expectedmapConfig);
+    });
+
+    it('should not overwrite mapConfig with custom paramters', async () => {
+      const mapOptions = {
+        vectorExtent: 2048,
+        vectorSimplifyExtent: 512,
+        bufferSize: {
+          mvt: 30
+        },
+        metadata: {
+          geometryType: true,
+          columnStats: {
+            topCategories: 5,
+            includeNulls: false
+          }
+        },
+        aggregation: {
+          placement: 'centroid',
+          resolution: 2,
+          threshold: 2
+        }
+      };
+
+      const source = new CARTOSource(DEFAULT_DATASET, { mapOptions });
+      await source.init({ sample: new Set(), aggregation: new Set() });
+
+      const expectedmapConfig = {
+        vectorExtent: 2048,
+        vectorSimplifyExtent: 512,
+        bufferSize: {
+          mvt: 30
+        },
+        dataset: DEFAULT_DATASET,
+        metadata: {
+          geometryType: true,
+          columnStats: {
+            topCategories: 5,
+            includeNulls: false
+          },
+          dimensions: true,
+          sample: {
+            num_rows: 1000,
+            include_columns: []
+          }
+        },
+        aggregation: {
+          columns: {},
+          dimensions: {},
+          placement: 'centroid',
+          resolution: 2,
+          threshold: 2
+        }
+      };
+
+      expect(instantiateMapFromMock.mock.calls[0][0]).toMatchObject(expectedmapConfig);
+    });
+  });
 });
