5bde8c0e810d912144df9f20e0fc824680a34131
Simon Martín <oleurud@gmail.com>
2020-07-03 16:10:29 +0200

    ensuring reintantiation keeps fields

--- src/lib/viz/source/__tests__/DatasetSource.spec.ts
+++ src/lib/viz/source/__tests__/DatasetSource.spec.ts
@@ -76,7 +76,17 @@ describe('DatasetSource', () => {
               {
                 meta: {
                   stats: {
-                    geometryType: 'Point'
+                    geometryType: 'Point',
+                    columns: {
+                      column1: {
+                        type: 'string',
+                        categories: []
+                      },
+                      column2: {
+                        type: 'string',
+                        categories: []
+                      }
+                    }
                   }
                 }
               }
@@ -119,7 +129,7 @@ describe('DatasetSource', () => {
         sql: `SELECT * FROM ${DEFAULT_DATASET}`
       };
 
-      expect(instantiateMapFromMock.mock.calls[0][0]).toMatchObject(expectedmapConfig);
+      expect(instantiateMapFromMock.mock.calls[0][0]).toStrictEqual(expectedmapConfig);
     });
 
     it('should overwrite default mapConfig with custom paramters', async () => {
@@ -174,7 +184,78 @@ describe('DatasetSource', () => {
         sql: `SELECT * FROM ${DEFAULT_DATASET}`
       };
 
-      expect(instantiateMapFromMock.mock.calls[0][0]).toMatchObject(expectedmapConfig);
+      expect(instantiateMapFromMock.mock.calls[0][0]).toStrictEqual(expectedmapConfig);
+    });
+
+    it('should add new dimensions and sample columns without removing previous ones', async () => {
+      const mapOptions = {
+        vectorExtent: 2048,
+        vectorSimplifyExtent: 512,
+        bufferSize: {
+          mvt: 30
+        },
+        metadata: {
+          geometryType: true,
+          columnStats: {
+            topCategories: 5,
+            includeNulls: false
+          },
+          sample: {
+            num_rows: 1000,
+            include_columns: ['column1']
+          }
+        },
+        aggregation: {
+          dimensions: {
+            column1: {
+              column: 'column1'
+            }
+          },
+          placement: 'centroid',
+          resolution: 2,
+          threshold: 2
+        }
+      };
+
+      const source = new DatasetSource(DEFAULT_DATASET, { mapOptions });
+      await source.init({ sample: new Set(['column2']), aggregation: new Set(['column2']) });
+
+      const expectedmapConfig = {
+        vectorExtent: 2048,
+        vectorSimplifyExtent: 512,
+        bufferSize: {
+          mvt: 30
+        },
+        metadata: {
+          geometryType: true,
+          columnStats: {
+            topCategories: 5,
+            includeNulls: false
+          },
+          dimensions: true,
+          sample: {
+            num_rows: 1000,
+            include_columns: ['column1', 'column2']
+          }
+        },
+        aggregation: {
+          columns: {},
+          dimensions: {
+            column1: {
+              column: 'column1'
+            },
+            column2: {
+              column: 'column2'
+            }
+          },
+          placement: 'centroid',
+          resolution: 2,
+          threshold: 2
+        },
+        sql: `SELECT * FROM ${DEFAULT_DATASET}`
+      };
+
+      expect(instantiateMapFromMock.mock.calls[0][0]).toStrictEqual(expectedmapConfig);
     });
   });
 });
