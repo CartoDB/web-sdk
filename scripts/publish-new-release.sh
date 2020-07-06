404e9e7c5ae59c3af2c70411e3760fed39f80ee0
Víctor Velarde <victor.velarde@gmail.com>
2020-07-06 02:52:25 +0200

    chore: add scripts for official release (to npm & cdn)
    A setup with release-it

--- scripts/publish-new-release.sh
+++ scripts/publish-new-release.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-BRANCH=$(git branch | grep -e "^*" | tr -d '* ';)
+VERSION=$(node --eval "console.log(require('./package.json').version);")
 
 printf "\n"
-echo -e "Ready to publish to the CDN Web SDK branch with name \033[1m$BRANCH\033[0m"
+echo -e "Ready to publish to the CDN Web SDK version \033[1m$VERSION\033[0m"
+echo -e "This procedure assumes that the version has already been published to npm"
 read -n1 -r -p ">> PRESS Ctrl+C to cancel, or any other key to continue..." key
 
 echo -e "Uploading to CDN..."
 
 printf "\n"
 echo -e "\t# Step 1/2 Publish"
-node scripts/cdn/publish.js 'current_branch' $@
+node scripts/cdn/publish.js 'release' $@
 
 printf "\n"
 echo -e "\t# Step 2/2 Invalidate"
