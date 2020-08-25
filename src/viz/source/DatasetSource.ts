3c4eecd740f1add8a40b1e40ac5d28d2cf7946d5
Víctor Velarde <victor.velarde@gmail.com>
2020-08-25 13:51:24 +0200

    refactor: rename source options param

--- src/viz/source/DatasetSource.ts
+++ src/viz/source/DatasetSource.ts
@@ -1,10 +1,10 @@
-import { SQLSource, SourceOptions } from './SQLSource';
+import { SQLSource, SQLSourceOptions } from './SQLSource';
 
 /**
  * Implementation of a Source compatible with CARTO's MAPs API
  * * */
 export class DatasetSource extends SQLSource {
-  constructor(dataset: string, options: SourceOptions = {}) {
+  constructor(dataset: string, options: SQLSourceOptions = {}) {
     super(`SELECT * FROM ${dataset}`, options);
 
     this.id = `CARTO-${dataset}`;
--- src/viz/source/SQLSource.ts
+++ src/viz/source/SQLSource.ts
@@ -16,12 +16,12 @@ import { sourceErrorTypes, SourceError } from '../errors/source-error';
 import { FiltersCollection } from '../filters/FiltersCollection';
 import { ColumnFilters } from '../filters/types';
 
-export interface SourceOptions {
+export interface SQLSourceOptions {
   credentials?: Credentials;
   mapOptions?: MapOptions;
 }
 
-const DEFAULT_ID_PROPERTY = 'cartodb_id';
+export const DEFAULT_ID_PROPERTY = 'cartodb_id';
 
 export const defaultMapOptions: MapOptions = {
   vectorExtent: 2048,
@@ -71,7 +71,7 @@ export class SQLSource extends Source {
     SQLFilterApplicator
   );
 
-  constructor(sql: string, options: SourceOptions = {}) {
+  constructor(sql: string, options: SQLSourceOptions = {}) {
     super(`CARTO-SQL-${uuidv4()}`);
     this.sourceType = 'SQLSource';
 
