0115dd142c7724dae531c09e98774d0ea1972d79
Javier Aragón <javier.aragon@geographica.gs>
2020-08-25 10:40:10 +0200

    feat: demo BQ

--- src/tilejson/TileJsonClient.ts
+++ src/tilejson/TileJsonClient.ts
@@ -5,22 +5,22 @@ import { TileJsonInstance } from './TileJsonInstance';
 export class TileJsonClient {
   public async getTilesetInfoFrom(url: string): Promise<TileJsonInstance> {
     // eg: https://bq1.cartocdn.com/tilesjson?t=cartobq.maps.ais_tileset
-    const r = await fetch(url);
-    return parseFetchJSON(r);
-
-    // return {
-    //   tiles: [
-    //     'https://bq1.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_14_0_5458_0_8191_4000_1&t=cartobq.maps.ais_tileset',
-    //     'https://bq2.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_14_0_5458_0_8191_4000_1&t=cartobq.maps.ais_tileset',
-    //     'https://bq3.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_14_0_5458_0_8191_4000_1&t=cartobq.maps.ais_tileset',
-    //     'https://bq4.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_14_0_5458_0_8191_4000_1&t=cartobq.maps.ais_tileset'
-    //   ],
-    //   vector_layers: [
-    //     {
-    //       geometry_type: 'Polygon'
-    //     }
-    //   ]
-    // };
+    // const r = await fetch(url);
+    // return parseFetchJSON(r);
+    //
+    return {
+      tiles: [
+        'https://bq1.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2',
+        'https://bq2.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2',
+        'https://bq3.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2',
+        'https://bq4.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2'
+      ],
+      vector_layers: [
+        {
+          geometry_type: 'Polygon'
+        }
+      ]
+    };
   }
 }
 
