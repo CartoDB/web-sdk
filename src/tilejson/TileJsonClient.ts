0382e9a91df9011a9c1a9181742361080d9f890c
Víctor Velarde <victor.velarde@gmail.com>
2020-08-26 17:25:26 +0200

    feat: dynamic connection to bq source tilejson endpoint

--- src/tilejson/TileJsonClient.ts
+++ src/tilejson/TileJsonClient.ts
@@ -5,33 +5,18 @@ import { TileJsonInstance } from './TileJsonInstance';
 export class TileJsonClient {
   public async getTilesetInfoFrom(url: string): Promise<TileJsonInstance> {
     // eg: https://bq1.cartocdn.com/tilesjson?t=cartobq.maps.ais_tileset
-    // const r = await fetch(url);
-    // return parseFetchJSON(r);
-    //
-    console.log(url);
-    return {
-      tiles: [
-        'https://bq1.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2',
-        'https://bq2.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2',
-        'https://bq3.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2',
-        'https://bq4.cartocdn.com/bqtiler?y={y}&x={x}&z={z}&p=0_16_19257_19396_24574_24697_4000_1&t=cartobq.maps.nyc_taxi_demo2'
-      ],
-      vector_layers: [
-        {
-          geometry_type: 'Polygon'
-        }
-      ]
-    };
+    const r = await fetch(url);
+    return parseFetchJSON(r);
   }
 }
 
-// function parseFetchJSON(r: Response) {
-//   switch (r.status) {
-//     case 200:
-//       return r.json();
-//     case 404:
-//       throw new Error('Not found tilejson for that url');
-//     default:
-//       throw new Error('Unexpected error fetching the tileset info');
-//   }
-// }
+function parseFetchJSON(r: Response) {
+  switch (r.status) {
+    case 200:
+      return r.json();
+    case 404:
+      throw new Error('Not found tilejson for that url');
+    default:
+      throw new Error('Unexpected error fetching the tileset info');
+  }
+}
